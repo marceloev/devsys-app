2b8083edef683f21126bb67847aeb7bc70cc3efd
Marcelo Costa Domingues <marceloevo@gmail.com>
2019-12-17 17:00:20 -0300

    maintence
    maintence

--- src/components/commom/series/Series.vue
+++ src/components/commom/series/Series.vue
@@ -2,6 +2,20 @@
     <dvs-dynaform name="Series" service-name="/series" instancia="Séries" :metadata="metadata" :rules="rules">
       <div slot="table-data-cols">
         <el-table-column prop="codigo" label="#" align="center" fixed="left" width="70" sortable></el-table-column>
+        <el-table-column prop="descricao" label="Descrição" align="center" sortable></el-table-column>
+        <el-table-column prop="nome" label="Nome" align="center" sortable></el-table-column>
+        <el-table-column label="Data/Hora Prim. Episódio" align="center">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span v-text="$formatDate(scope.row.dataPrimeiroEpisodio, '(Não Definido)')"></span>
+          </template>
+        </el-table-column>
+        <el-table-column label="Data/Hora Ult. Episódio" align="center">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span v-text="$formatDate(scope.row.dataUltimoEpisodio, '(Não Definido)')"></span>
+          </template>
+        </el-table-column>
       </div>
     </dvs-dynaform>
 </template>
