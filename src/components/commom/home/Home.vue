7c98c5f8cac7dac52e2c6a25458fb7b4c565de93
Emerson Ribeiro Junior <emerson.junior@sankhya.com.br>
2019-12-30 18:03:57 -0300

    Adding beginning of card structure

--- src/components/commom/home/Home.vue
+++ src/components/commom/home/Home.vue
@@ -1,7 +1,5 @@
 <template>
-    <div id="background" class="h-100">
-
-    </div>
+    <div class="card card-1"></div>
 </template>
 
 <script>
@@ -11,5 +9,28 @@ export default {
 </script>
 
 <style scoped>
-#background { background-color: red; }
+body {
+  background: #e2e1e0;
+  text-align: center;
+}
+
+.card {
+  background: #fff;
+  border-radius: 2px;
+  display: inline-block;
+  height: 300px;
+  margin: 1rem;
+  position: relative;
+  width: 300px;
+}
+
+.card-1 {
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+}
+
+.card-1:hover {
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+}
+
 </style>
